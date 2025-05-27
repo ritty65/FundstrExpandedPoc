f9bd51baa6415d57885de6debb67ac67da70493d
Simu <Simulacraproductions1@gmail.com>
2025-05-27 12:55:04 +0200

    Clean up: remove node_modules and dist from tracking, add .gitignore

---
+++ packages/web-ui/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Scheduler Web UI</h1>
+      <p>Your React app is running!</p>
+    </div>
+  );
+}
